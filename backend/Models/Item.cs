ffae210737b4502eaa9044e20125ee5d8e78c1f8
luther <dzadan73@gmail.com>
2025-09-07 20:01:50 +0200

    route for getting user`s file and straight downloading

---
+++ backend/Models/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CloudCore.Models;
+
+public partial class Item
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Type { get; set; } = null!;
+
+    public int? ParentId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string? FilePath { get; set; }
+
+    public long? FileSize { get; set; }
+
+    public string? MimeType { get; set; }
+
+    public bool? IsDeleted { get; set; }
+
+    public virtual ICollection<Item> InverseParent { get; set; } = new List<Item>();
+
+    public virtual Item? Parent { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,17 +1,39 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using CloudCore.Models;
+using System;
 
 namespace CloudCore
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            DotNetEnv.Env.Load("../.env");
+
+
+
+            // before building docker change it to DB_HOST
+            var host = Environment.GetEnvironmentVariable("DB_HOST");
+            var port = Environment.GetEnvironmentVariable("DB_PORT");
+            var database = Environment.GetEnvironmentVariable("DB_NAME");
+            var user = Environment.GetEnvironmentVariable("DB_USER");
+            var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            var connectionString = $"Server={host};Port={port};Database={database};Uid={user};Pwd={password};";
             var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddDbContext<CloudCoreDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen();
+
+
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll", policy =>
@@ -23,8 +45,23 @@ namespace CloudCore
             });
 
             var app = builder.Build();
+
+
+           
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
             app.UseCors("AllowAll");
             app.UseRouting();
+
+            app.UseAuthorization();
+
+            
             app.MapControllers();
             app.Run("http://0.0.0.0:5000");
         }
